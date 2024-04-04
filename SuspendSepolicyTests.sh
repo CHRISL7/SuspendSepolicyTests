c2e318ac297014ffbeb87626c107507581ac24fb
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-04-04 16:57:05 +0000

    SuspendSepolicy: Remove API 33 check
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- SuspendSepolicyTests.sh
+++ SuspendSepolicyTests.sh
@@ -75,11 +75,6 @@ pass() { #msg
     echo $1
     exit 0
 }
-# requirement added in T (33)
-vendor_api_level="$(adb shell getprop ro.vendor.api_level 33)"
-if [ "$vendor_api_level" -lt 33 ]; then
-    pass "Test skipped: vendor_api_level ($vendor_api_level) < min_api_level (33)"
-fi
 # Test unlabeled sysfs_wakeup nodes
 wakeup_paths+=( $(get_wakeup_paths) )
 get_unlabeled_wakeup_paths
